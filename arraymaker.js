cb42c8902b6112e7a5a3b520094cade0086db5e9
seanobada <56548428+seanobada@users.noreply.github.com>
2020-03-11 16:22:44 +0000

    Add files via upload

--- arraymaker.js
+++ arraymaker.js
@@ -1,9 +1,9 @@
 var fs = require( 'fs' )
 function writeToFile(value1) {
-fs.appendFileSync( './array5000.csv' ,value1+ "," + "\n" )
+fs.appendFileSync( './array500000b.csv' ,value1+ "," + "\n" )
 }
 
-let i = Array.from({length: 5000}, ()=> Math.floor(Math.random() * 5000))
+let i = Array.from({length: 500000}, ()=> Math.floor(Math.random() * 500000))
 console.log(i)
 
 writeToFile(i)
